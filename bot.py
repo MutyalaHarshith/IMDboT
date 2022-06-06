f1d90c96b138cf13fb7ed51bc50acc5da178c840
ముత్యాల హర్షిత్ <91818980+MutyalaHarshith@users.noreply.github.com>
2022-06-06 14:36:29 +0530

    Create bot.py

---
+++ bot.py
@@ -0,0 +1,74 @@
+# -*- coding: utf-8 -*-
+"""
+@MutyalaHarshith Was the
+creator Of this BoT
+"""
+import logging
+from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
+import urllib.request
+import json
+import imdb
+import os
+PORT = int(os.environ.get('PORT', 5000))
+api_key= #Enter your Telegram bot token
+ia = imdb.IMDb() 
+logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+                    level=logging.INFO)
+logger = logging.getLogger(__name__)
+
+
+def start(update, context):
+    update.message.reply_text('Hi! \nWelcome to the *IMDb Bot*. \nఏదైనా సినిమా వివరాలను పొందడానికి దాని పేరును నాకు పంపండి. \nThanks for using! \n \nCreated by [Mutyala Harshith](https://t.me/MutyalaHarshith)',parse_mode='markdown')
+
+
+def help(update, context):
+    update.message.reply_text('ఏదైనా సినిమా వివరాలను పొందడానికి దాని పేరును నాకు పంపండి @MutyalaHarshith. \nప్రయత్నించండి "సర్కారు వారి పాట"')
+
+def error(update, context):
+    logger.warning('Update "%s" caused error "%s"', update, context.error)
+
+def reply(update, context):
+    movie_name=update.message.text
+    search = ia.search_movie(movie_name) 
+      
+    id='tt'+search[0].movieID
+    
+    url= 'http://www.omdbapi.com/?i='+id+'&apikey='+api_key
+    
+    x=urllib.request.urlopen(url)
+    
+    for line in x:
+        x=line.decode()
+    
+    data=json.loads(x)
+    
+    ans='@MutyalaHarshith'
+    ans+='*'+data['Title']+'* ('+data['Year']+')'+'\n\n'
+    ans+='*IMDb Rating*: '+data['imdbRating']+' \n'
+    ans+='*Cast*: '+data['Actors']+'\n'
+    ans+='*Genre*: '+data['Genre']+'\n\n'
+    ans+='*Plot*: '+data['Plot']+'\n'
+    ans+='[.]('+data['Poster']+')'
+    update.message.reply_text(ans,parse_mode='markdown')  
+
+
+def main():
+
+    updater = Updater("1476373283:AAHrdQE394_J8qd78J974y_AwVdwrsis1r0", use_context=True)
+    dp = updater.dispatcher
+    
+    dp.add_handler(CommandHandler("start", start))
+    dp.add_handler(CommandHandler("help", help))
+    dp.add_handler(MessageHandler(Filters.text, reply))
+    dp.add_error_handler(error)
+
+    #updater.start_webhook(listen="0.0.0.0",
+    #                      port=int(PORT),
+    #                      url_path="1476373283:AAHrdQE394_J8qd78J974y_AwVdwrsis1r0")
+    #updater.bot.setWebhook('https://imdb-movie-bot.herokuapp.com/' + "1476373283:AAHrdQE394_J8qd78J974y_AwVdwrsis1r0")
+    updater.start_polling()
+    updater.idle()
+
+
+if __name__ == '__main__':
+    main() 
